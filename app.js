648b5fda4d8e58e3d826e824887fbac8bff9acfa
Marta Rosa <marta.r.93@gmail.com>
2022-09-13 23:25:03 +0200

    Updated 2nd section

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const hamburgerButtonOpen = document.querySelector(".hamburger_open");
 const hamburgerButtonClose = document.querySelector(".hamburger_close");
+const body = document.querySelector("body");
 
 hamburgerButtonOpen.addEventListener("click", () => {
   const navigation = document.querySelector(".navigation");
 
+  body.style.overflow = "hidden";
   navigation.classList.add("navigation_open");
 });
 
 hamburgerButtonClose.addEventListener("click", () => {
   const navigation = document.querySelector(".navigation");
 
+  body.style.overflow = "auto";
   navigation.classList.remove("navigation_open");
 });
